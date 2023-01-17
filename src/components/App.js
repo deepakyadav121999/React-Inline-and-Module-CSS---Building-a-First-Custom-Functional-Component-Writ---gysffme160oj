ad95f69ff1741cbea5245dcf8d6015286ae0eb60
deepakyadav121999 <dy44444221@gmail.com>
2023-01-17 18:17:06 +0530

    fpp

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../styles/App.css';
-import styles from '../styles/appStyles.module.css';
+import styles from'../styles/appStyles.module.css';
 const App = () => {
 const heading ={
 
@@ -8,7 +8,7 @@ const heading ={
 }
   return (
     <div id="main">
-      <h1 >Success</h1>
+      <h1 className ={styles.success}>Success</h1>
       <h1 id='error' >Error</h1>
     </div>
   )
